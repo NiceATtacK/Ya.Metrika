6da950245dc6d7d88aaefc48b3fabee94ace889c
Center-WD <46905310+Center-WD@users.noreply.github.com>
2020-04-09 11:23:38 +0300

    Добавлены цели на моб. телефоны

--- goals.js
+++ goals.js
@@ -1,10 +1,29 @@
 /*
 *  Цели метрики 49711711
-*  goal - @string
-*  setGoal('goal');
+*  goal:  @string
+*  Use: setGoal('goal');
 */
 function setGoal(object){
     yaCounter49711711.reachGoal(object);
+    console.log('PageView: 49711711 Params '+object); // Логируем срабатывание цели
     return true;
 }
+
+/*
+* Кнопка номера телефона +7(863) 303 51 55
+*/
+$( 'a[href="tel:+7(863)303-51-55"]' ).on( "click", function(event) {
+    setGoal('phone_1');
+    $( this ).off(event);
+    return true;
+});
+
+/*
+* Кнопка номера телефона +7(905)451-06-11
+*/
+$( 'a[href="tel:+7(905)451-06-11"]' ).on( "click", function(event) {
+    setGoal('phone_2');
+    $( this ).off(event);
+    return true;
+});
 
